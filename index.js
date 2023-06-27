e055ffb692180de4c98d031baa8d17ed1e3980fe
RisellU <rcum22@gmail.com>
2023-06-26 19:06:12 -0500

    funciones de preguntar por ruta absoluta/ Convertir a absoluta/ validar que existe

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-module.exports = () => {
-  // ...
-};
+// const { isAbsolute } = require(".");
+
+// function mdLinks(route) {
+//     return isAbsolute(route)
+// }
+// console.log(mdLinks('C:/Users/Otros/Desktop/MD-LINKS/DEV006-md-links/test/prueba'));
